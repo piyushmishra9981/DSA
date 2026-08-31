1192cff21fe9f245b2d27c21ce0f19f8b08b4c37
Piyush mishra <piyushmishra13527@gmail.com>
2026-08-31 18:04:55 +0530

    Update pattern1.cpp

--- basics/pattern1.cpp
+++ basics/pattern1.cpp
@@ -104,23 +104,6 @@ void print7(int n){
 
         cout<<endl;
     }
-
-    //star
-
-    for (int j = 0; j <2*i+1; j++)
-    {
-        cout<<"*";
-    }
-    
-     //space
-
-    for (int j = 0; j <n-i-1; j++)
-    {
-        cout<<" ";
-    }
-
-    cout<<endl;
-}
     
 }
 void print8(int n){
@@ -160,11 +143,6 @@ int main(){
         int n;
         cin>>n;
         print8(n);
-    }
-    
-
-        print7(n);
-
     }
 
   return 0;  
