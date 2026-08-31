91b81b10ad322dd3725212cea89226ddc909521c
Piyush mishra <piyushmishra13527@gmail.com>
2026-08-31 17:46:19 +0530

    initial commit

--- basics/pattern1.cpp
+++ basics/pattern1.cpp
@@ -97,13 +97,41 @@ void print7(int n){
         //space
         for (int j = 0; j <n-i-1; j++)
         {
-           cout<<"*"; 
+           cout<<" "; 
         }
 
         cout<<endl;
     }
     
 }
+void print8(int n){
+    for (int i = 0; i < n; i++)
+    {
+        //space
+        for (int j = 0; j <i; j++)
+        {
+           cout<<" " ;
+        }
+        
+
+        //star
+        for (int j = 0; j < 2*n-(2*i+1); j++)
+        {
+            cout<<"*";
+        }
+        
+
+        //space
+        for (int j = 0; j <i; j++)
+        {
+           cout<<" "; 
+        }
+
+        cout<<endl;
+    }
+    
+}
+
 
 int main(){
     int t;
@@ -112,7 +140,7 @@ int main(){
     {
         int n;
         cin>>n;
-        print7(n);
+        print8(n);
     }
     
 
