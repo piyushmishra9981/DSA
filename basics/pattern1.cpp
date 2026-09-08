7cbb2c3bd1695f7823b3c6b3f44668c1d6161f18
piyushmishra13527@gmail.com <piyushmishra13527@gmail.com>
2026-09-08 17:09:05 +0530

    Add pattern1 code

--- basics/pattern1.cpp
+++ basics/pattern1.cpp
@@ -142,10 +142,29 @@ void print10(int n){
     for (int i = 1; i <= 2*n-1 ; i++)
     {
         int stars = i;
+
         if( i > n ) stars = 2*n-i;
+        
         for (int j = 1; j <= stars; j++)
         {
             cout<<"*";
+        }
+        cout<<endl;
+        
+        
+    }
+    
+}
+void print11(int n){
+    int start = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (i%2 == 0) start = 1;
+        else start = 0;
+        for (int j = 0; j <=i; j++)
+        {
+            cout<<start;
+            start = 1 - start;
         }
         cout<<endl;
         
@@ -162,7 +181,7 @@ int main(){
     {
         int n;
         cin>>n;
-        print10(n);
+        print11(n);
     }
 
   return 0;  
