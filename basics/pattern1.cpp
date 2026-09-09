c186bf3853ee2fc36351fa97bc316d474b93a210
piyushmishra13527@gmail.com <piyushmishra13527@gmail.com>
2026-09-09 18:21:29 +0530

    initial commit

--- basics/pattern1.cpp
+++ basics/pattern1.cpp
@@ -222,6 +222,64 @@ void print14(int n){
     }
     
 }
+void print15(int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (char ch = 'A'; ch <='A' + (n-i-1); ch++)
+        {
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+        
+    }
+    
+}
+void print16(int n){
+    for (int i = 0; i < n; i++)
+    {
+        char ch = 'A' + i;
+        for (int j = 0; j <=i; j++)
+        {
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+        
+    
+    }
+}
+void print17(int n){
+    for (int i = 0; i < n; i++){
+        //spaces
+        for (int j = 0; j < n-i-1; j++)
+        {
+            cout<<" ";
+        }
+
+        //characters
+
+        char ch = 'A';
+        int breakpoint = (2*i+1)/2;
+        for (int j = 1; j <= 2*i+1; j++)
+        {
+            cout<<ch;
+            if (j<=breakpoint) ch++;
+            else ch--;
+            
+            
+        }
+
+        //spaces
+
+        for (int j = 0; j < n-i-1; j++)
+        {
+            cout<<" ";
+        }
+        cout<<endl;
+        
+    }
+    
+    
+}
 
 
 int main(){
@@ -231,7 +289,7 @@ int main(){
     {
         int n;
         cin>>n;
-        print14(n);
+        print17(n);
     }
 
   return 0;  
